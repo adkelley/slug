f5d5ae1c99d73cb266f1eea43f30685a56710084
Alex Kelley <adkelley@users.noreply.github.com>
2021-03-31 17:10:23 -0700

    slug, url converted to map

--- lib/slug.ex
+++ lib/slug.ex
@@ -7,12 +7,16 @@ defmodule Slug do
 
   @spec main(String.t()) :: String.t()
   def main(_args) do
+   %{url: url, slug: slug} = url_slug()
+    "Read [#{slug}](#{url}) #[[Blog Posts]]" |> Clipboard.copy()
+    :timer.sleep(100)
+  end
+
+  @spec url_slug()  :: %{url: String.t(), slug: String.t()}
+  defp url_slug() do
     url = Clipboard.paste()
     slug = url |> get_slug()
-    
-    "Read [#{slug}](#{url}) #[[Blog Posts]]" |> Clipboard.copy()
-
-    :timer.sleep(100)
+    %{url: url, slug: slug}
   end
 
 
